b8a006c02ef2b2e0cc0bc1895372a2555c61387f
David Xu <davidxu1991@gmail.com>
2017-12-03 22:49:20 +1300

    finished the masters screen

--- js/components/MasterView.js
+++ js/components/MasterView.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Profiles from "../Profiles";
+import styles from './masterStyles';
+
 import {
     Container,
     Header,
@@ -13,33 +15,93 @@ import {
     Thumbnail,
     Left,
     Body,
-    Right
+    Right,
+    Item,
+    Input
 } from "native-base";
 
 export default class MasterView extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showSearch: false,
+            searchTerm: ""
+        }
+
+        this.showSearch = this.showSearch.bind(this);
+        this.hideSearch = this.hideSearch.bind(this);
+        this.inputSearchTerm = this.inputSearchTerm.bind(this);
+    }
+
+    showSearch = () => {
+        console.log('showing search');
+        this.setState({ showSearch: true });
+    }
+
+    hideSearch = () => {
+        console.log('hiding search');
+        this.setState({ showSearch: false, searchTerm: "" });
+    }
+
+    inputSearchTerm = (text) => {
+        console.log(text);
+        this.setState({ searchTerm: text })
+    }
+
     render() {
         const nav = this.props.navigation.navigate;
-
+        const filteredProfiles = Profiles.filter(p => {
+            return this.state.searchTerm === "" ? p : p.Name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+        });
         console.log("rendering Master view");
         return (
-            <Container>
-                <Header>
-                    <Body>
-                        <Title>Spark Jedi</Title>
-                    </Body>
+            <Container style={styles.container}>
+                <Header searchBar rounded >
+
+                    {this.state.showSearch &&
+                        <Item>
+                            <Icon active name="search" />
+                            <Input placeholder="Search" onChangeText={this.inputSearchTerm} />
+                            <Button transparent onPress={this.hideSearch}>
+                                <Icon active name="close" />
+                            </Button>
+                        </Item>
+                    }
+
+                    {!this.state.showSearch &&
+                        <Left>
+                            <Icon name="people" />
+                        </Left>
+                    }
+
+                    {!this.state.showSearch &&
+                        <Body>
+                            <Title>Spark Jedi</Title>
+                        </Body>
+                    }
+
+                    {!this.state.showSearch &&
+                        <Right>
+                            <Button transparent onPress={this.showSearch}>
+                                <Icon name="search" />
+                            </Button>
+                        </Right>
+                    }
+
                 </Header>
 
                 <Content>
                     <List
-                        dataArray={Profiles}
+                        dataArray={filteredProfiles}
                         renderRow={(Profile, SectionID, RowID) =>
-                            <ListItem thumbnail onPress={() => nav('DetailView', { Profile })}>
+                            <ListItem style={styles.listItem} thumbnail onPress={() => nav('DetailView', { Profile })}>
                                 <Left>
                                     <Thumbnail circular size={55} source={Profile.img} />
                                 </Left>
                                 <Body>
                                     <Text>{Profile.Name}</Text>
-                                    <Text numberOfLines={2} note>{Profile["Your why?"]}</Text>
+                                    <Text numberOfLines={2} note>{Profile.Why}</Text>
                                 </Body>
                                 <Right>
                                     <Icon name="arrow-forward" />
---
+++ js/components/masterStyles.js
@@ -0,0 +1,19 @@
+const React = require("react-native");
+
+const { StyleSheet } = React;
+
+export default {
+    container: {
+        backgroundColor: "#ededed"
+    },
+    listItem: {
+        backgroundColor: "#ededed",
+    },
+    text: {
+        alignSelf: "center",
+        marginBottom: 7
+    },
+    mb: {
+        marginBottom: 15
+    }
+};
